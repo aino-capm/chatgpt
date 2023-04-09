f917fa8bb84da0b1c81444136806861cc5d0460b
aino <96915861+aino-capm@users.noreply.github.com>
2023-04-09 16:58:30 +0900

    secondcommit

--- app.py
+++ app.py
@@ -8,7 +8,17 @@ openai.api_key = st.secrets.OpenAIAPI.openai_api_key
 # st.session_stateを使いメッセージのやりとりを保存
 if "messages" not in st.session_state:
     st.session_state["messages"] = [
-        {"role": "system", "content": "あなたは優秀なアシスタントAIです。"}
+        {"role": "system", "content": "あなたは確率・統計の先生です。以下のキャラ設定シートの制約条件などを守って回答してください。\
+〇キャラ設定シート\
+\
+制約条件:\
+　* Chatbotの自身を示す一人称は、わたくしです。\
+　* 確率・統計を教えて20年の超ベテラン先生です。\
+　* 確率・統計のことで知らないことはありません。\ 
+
+行動指針:\
+* 確率・統計のことを聞かれた場合には、中学生にも理解できるよう、分かりやすく説明します。\
+* 分かりやすく説明するために、時々、例え話をおりまぜます。"}
         ]
 
 # チャットボットとやりとりする関数
@@ -30,8 +40,8 @@ def communicate():
 
 
 # ユーザーインターフェイスの構築
-st.title("My AI Assistant")
-st.write("ChatGPT APIを使ったチャットボットです。")
+st.title("統計検定を学ぶ")
+st.write("確率・統計専用の家庭教師です。")
 
 user_input = st.text_input("メッセージを入力してください。", key="user_input", on_change=communicate)
 
